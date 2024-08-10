54265a4515f7b095587956f7d9afa04589214b77
tusharmishra069 <mishratushar133@gmail.com>
2024-08-10 10:57:54 +0530

    product of 3 no.

---
+++ productof3no.cpp
@@ -0,0 +1,17 @@
+// ques get three float no. as input find the product and print it 
+#include<iostream>
+using namespace std;
+
+int main(){
+    float num1,num2,num3;
+    cout<<"enter the first no.  :-";
+    cin>>num1;
+    cout<<"enter the second no.  :-";
+    cin>>num2;
+    cout<<"enter the third no.  :-";
+    cin>>num3;
+  
+    float prouduct = num1*num2*num2;
+
+    cout<<prouduct;
+}
