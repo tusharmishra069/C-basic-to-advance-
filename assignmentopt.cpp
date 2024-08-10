90328d57adfa07f579eb4aab6a84d092c6598aca
tusharmishra069 <mishratushar133@gmail.com>
2024-08-10 11:08:09 +0530

    assignment operator

---
+++ assignmentopt.cpp
@@ -0,0 +1,17 @@
+//assignment operators example
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10;
+    n+=100;
+    cout<<n<<endl;//110
+    n-=100;
+    cout<<n<<endl;//100
+    n*=2;
+    cout<<n<<endl;//200
+    n/=5;
+    cout<<n<<endl;//4
+    n%=3;
+    cout<<n;//1
+
+}
