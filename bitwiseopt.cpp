152a15126d8ae2c236edb7927d71c99f3663c1e4
tusharmishra069 <mishratushar133@gmail.com>
2024-08-10 12:17:44 +0530

    bitwise operator

---
+++ bitwiseopt.cpp
@@ -0,0 +1,18 @@
+// bitwise or opretor
+#include<iostream>
+using namespace std;
+
+// 1. bitwise &
+// 2, bitwise |
+// 3. bitwise ~
+// 4. bitwise xor ^
+
+int main(){
+    cout<<(10&4)<<endl;
+    cout<<(11&9)<<endl;
+    cout<<(10|4)<<endl;
+    cout<<(11|9)<<endl;
+    cout<<(~10)<<endl; //negation of a no. is -(no.+1)
+    cout<<(10^4)<<endl;
+    cout<<(12^5)<<endl;//xor example
+}  
