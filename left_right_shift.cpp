d7af90ee74d8c9688e6975b82d7ab0c64278401a
tusharmishra069 <mishratushar133@gmail.com>
2024-08-11 21:27:14 +0530

    left shift right shift

---
+++ left_right_shift.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<(7<<1)<<endl;
+    cout<<(9<<1);
+
+// right shift
+    cout<<(2>>1)<<endl;
+    cout<<(5>>1)<<endl;
+    cout<<(8>>2)<<endl;
+}
