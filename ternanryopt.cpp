080d4537a9a20cfb016a35a2b6081d67bbcf86cc
tusharmishra069 <mishratushar133@gmail.com>
2024-08-12 10:56:16 +0530

    ternanry operator

---
+++ ternanryopt.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+//short hand if else (ternanry operator)
+
+int main(){
+    string name = "tushar";
+    string res=(name == "tushar")?"yes":"no";
+    cout<<res;
+}
