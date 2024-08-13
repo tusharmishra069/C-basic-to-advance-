450ef7eda73d173a88852161ef5c680ad7b9a680
tusharmishra069 <mishratushar133@gmail.com>
2024-08-13 23:54:51 +0530

    switch case

---
+++ basic of cpp/switch_case.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cout<<"enter ut choice";
+    cin>>n;
+    switch(n){
+        case 1:
+        cout<<"its one";
+        break;
+        case 10:
+        cout<<"its ten";
+        break;
+        default:
+        cout<<"default case";
+    }
+}
