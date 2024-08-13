b00ca1f27e53e2330b264285c8624e15bb0e7314
tusharmishra069 <mishratushar133@gmail.com>
2024-08-14 00:04:07 +0530

    function without arugment without return value

---
+++ basic of cpp/functions_1..cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+//functions 
+//types
+// 1. predefined/system defined/in built 
+// ex. tolower,int,cin,cout
+// 2. user defined
+
+//why fucntions ? :- to avoid the code repetition;
+
+// types pf user defined functon
+// 1. function without arugment  without return value
+// 2. withou argument with return value
+// 3. with argument without return value 
+// 4. with argument with return value
+
+// important terms
+//    function calll and the fucntion defining
+
+//type 1 :- function without arugment  without return value
+//function define
+void tushar(){
+   cout<<"give us party";
+}
+int main(){
+  tushar();//function calling
+}
+
