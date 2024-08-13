cb382b3b1adf4de950e3711e00bd918c1358fd52
tusharmishra069 <mishratushar133@gmail.com>
2024-08-14 00:26:40 +0530

    fucntion with argument without return value

---
+++ basic of cpp/function_3.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+// type 3 with argument without return value
+
+void tushar(int n, int n1){
+    if(n==7)
+    cout<<"just chillin";
+    else
+    cout<<"planning to chillin";
+}
+int main(){
+    tushar(20,200);
+}
