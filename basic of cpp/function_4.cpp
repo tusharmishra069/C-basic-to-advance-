0d5bd186e3a9a650fbf20ffe99354cc11dafb17a
tusharmishra069 <mishratushar133@gmail.com>
2024-08-14 00:28:20 +0530

    fucn with argu with return value

---
+++ basic of cpp/function_4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+// type 4 with argument with return value
+
+string tushar(int n, int n1){
+    if(n==7)
+    return "just chillin";
+    else
+    return"planning to chillin";
+}
+int main(){
+    tushar(20,200);
+}
