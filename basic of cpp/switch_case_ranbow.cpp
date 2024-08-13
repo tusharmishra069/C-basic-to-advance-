bcfdea634394d79d3580e882deaac0ee0ae6fdda
tusharmishra069 <mishratushar133@gmail.com>
2024-08-13 23:57:16 +0530

    ranbow choice and ouput acc to the initial

---
+++ basic of cpp/switch_case_ranbow.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace  std;
+
+//using a switch case make a ranbow choice that throws the output according to the initail
+
+int main(){
+    char c ;
+    cout<<"enter ut choice:- ";
+    cin>>c;
+    c = tolower(c);
+    switch(c){
+        case 'v':
+        cout<<"its violet";
+        break;
+        case 'i':
+        cout<<"its indigo";
+        break;
+        case 'b':
+        cout<<"its blue";
+        break;
+        case 'g':
+        cout<<"its green";
+        break;
+        case 'y':
+        cout<<"its yellow";
+        break;
+        case 'o':
+        cout<<"its orange";
+        break;
+        case 'r':
+        cout<<"its red";
+        break;
+        default:
+        cout<<"default case";
+    }
+}
