ba6940e3f14bf2ca9649a7e8cb4e0aa0c9711606
tusharmishra069 <mishratushar133@gmail.com>
2024-08-16 15:54:12 +0530

    input from the user

---
+++ string/string_1.1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+using namespace std;
+// input from the user
+
+int main(){
+    string ourname;
+    string urname = "my student";// using string datatype
+    cout<<urname<<endl;
+    cout<<"enter the name:- ";//
+    cin>>ourname;
+    cout<<urname<<" "<<ourname;// this can only take one word that is given by the user if we want multiple words aur char we have different key word
+}
