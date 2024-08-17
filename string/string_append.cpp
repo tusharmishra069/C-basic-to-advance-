00676131c62e9b7efaa9894aa3876aebffc23398
tusharmishra069 <mishratushar133@gmail.com>
2024-08-17 13:26:22 +0530

    string append function

---
+++ string/string_append.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+// in built string fucntion
+
+int main(){
+    string s = "chocolate";
+    cout<<s.append(" is tasty")<<endl;// add more string to pervious stirng
+}
