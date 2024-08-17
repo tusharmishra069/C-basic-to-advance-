8a90e701824dfa7ab03a635e11fb5547971e220b
tusharmishra069 <mishratushar133@gmail.com>
2024-08-17 09:52:08 +0530

    length of the string

---
+++ string/string_length.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s = "chocolate";
+    cout<<s.length()<<endl;//show the lenght of string
+}
+
+// in built string fucntion
