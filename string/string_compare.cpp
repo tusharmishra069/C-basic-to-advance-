a6c891a0f3eee046623a76bc5a59f4a7a654693e
tusharmishra069 <mishratushar133@gmail.com>
2024-08-17 13:35:26 +0530

    string compare function

---
+++ string/string_compare.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string s1 = "a";
+    string s2 = "c";
+    cout<<s1.compare(s2)<<endl;// compare difference of the char in the alphabetical order
+}
