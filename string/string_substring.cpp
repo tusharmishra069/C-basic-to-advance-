cb259c32f31ebf9666340abf821c3f084a28a0e8
tusharmishra069 <mishratushar133@gmail.com>
2024-08-17 13:40:37 +0530

    added some more example

--- string/string_substring.cpp
+++ string/string_substring.cpp
@@ -4,4 +4,7 @@ using namespace std;
 int main(){
     string s = "chocolate";
     cout<<s.substr(0,3)<<endl;// to extract a substring from a string.
+    cout<<s.substr(0,4)<<endl;//its take the index value from zero to ending arugment that is given
+    cout<<s.substr(0)<<endl;//its take all the value from starting index to the end of the index
+    cout<<s.substr(4,4);// here we can give first argument as the index and second argument as the no. of char we want 
 }
