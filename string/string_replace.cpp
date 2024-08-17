923c9471dede4400a5193fee5672e32a849b4121
tusharmishra069 <mishratushar133@gmail.com>
2024-08-17 13:30:36 +0530

    string replace func

---
+++ string/string_replace.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+// in built string fucntion
+int main(){
+    string s = "chocolate";
+    cout<<s.replace(0,1,"school")<<endl;//replace the char we just need to decalre the range
+}
