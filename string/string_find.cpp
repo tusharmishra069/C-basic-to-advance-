f522f11623597bd0745bd11f567cf127ef5a1341
tusharmishra069 <mishratushar133@gmail.com>
2024-08-17 13:32:11 +0530

    string find function

---
+++ string/string_find.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+// in built string fucntion
+int main(){
+    string s = "chocolate";
+    cout<<s.find("h")<<endl;//returns the position of the first occurrence of a substring.
+}
