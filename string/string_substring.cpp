87328cea64da0efe6c22f795090f2b81947b2008
tusharmishra069 <mishratushar133@gmail.com>
2024-08-17 13:33:48 +0530

    to get the substring

---
+++ string/string_substring.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+// in built string fucntion
+int main(){
+    string s = "chocolate";
+    cout<<s.substr(0,3)<<endl;// to extract a substring from a string.
+}
