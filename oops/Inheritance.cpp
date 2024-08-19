5d6805b98d0c236f6ddd15599b630d0355c56139
tusharmishra069 <mishratushar133@gmail.com>
2024-08-19 12:09:04 +0530

    Inheritance

---
+++ oops/Inheritance.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+// Inheritance
+// - for efficency in terms of sharing properties between classes can be achive using inheritance
+
+// type:
+// 1.Single and simple inheritance
+// 2.multiple inheritance
+// 3.multi-level inheritance
+// 4.hybrid inheritance
+// 5.herarical inheritance
+
+
+class add{
+    public:
+    int n1,n2;
+
+};
+class multi:public add{
+    public:
+    int n3;
+    void mlt(){
+    int multi =(n1+n2)*n3 ;
+    cout<<multi;
+    }
+};
+
+int main(){
+    multi obj;
+    cout<<"enter three no. "<<endl;
+    cin>>obj.n1>>obj.n2>>obj.n3;
+    obj.mlt();
+}
