70a96efdcf170008c4fd4338ce9a6bfeb8425259
tusharmishra069 <mishratushar133@gmail.com>
2024-08-19 12:20:19 +0530

    Polymorphism, operator overloading

---
+++ oops/Polymorphism_1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+// Polymorphism
+
+// same thing use for different purposes.
+// one thing - different forms
+
+// types
+// -operator overloading
+// -method overloading
+// -overridding
+
+
+// operator overloading example
+int main(){
+    cout<<10+20<<endl;//Concatenation
+    string a="10",b="20";
+    cout<<a+b<<endl;//Concatenation
+    cout<<100+200;//Concatenation
+}
