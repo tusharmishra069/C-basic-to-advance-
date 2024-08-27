40ef1a39005761ed7e3da9fec79d060dc9138992
tusharmishra069 <mishratushar133@gmail.com>
2024-08-27 21:57:53 +0530

    bday gift

---
+++ project/birthday.cpp
@@ -0,0 +1,40 @@
+// • Unique Birthday Gift
+// Your birthday is coming soon and one of your friends, Alex, is thinking about a gift for you. He knows
+// that you really like integer arrays with interesting properties.
+// He selected two numbers, N and K and decided to write down on paper all integer arrays of length K
+// (in form a[1], a[2], ..., a[K]), where every number a[i] is in range from 1 to N, and, moreover, a[i+1] is
+// divisible by a[i] (where 1 < i <= K), and give you this paper as a birthday present.
+// Alex is very patient, so he managed to do this. Now you're wondering, how many different arrays are
+// written down on this paper?
+// Since the answer can be really large, print it modulo 10000.
+// Input:
+// The first line contains an integer, n, denoting the maximum possible value in the arrays.
+// The next line contains an integer, k, denoting the length of the arrays.
+#include<iostream>
+using namespace std;
+void birthday()
+{
+    int n;
+    cout<<"enter n"<<endl;
+    cin>>n;
+    int k;
+    cout<<"maximum length possible"<<endl;
+    cin>>k;
+    int count=0;
+    if(k==1){
+        cout<<n<<endl;
+    }
+    else{
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                if(j%i==0){
+                    count++;
+                }
+            }
+        }
+        cout<<count<<endl;
+    }
+}
+int main(){
+   birthday();
+}
