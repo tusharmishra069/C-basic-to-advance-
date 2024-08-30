dc09cec5b485bbf4ec41e860909e86ba300fdf6c
tusharmishra069 <mishratushar133@gmail.com>
2024-08-31 02:30:21 +0530

    stl list

---
+++ STL/List/list.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+// list
+
+
+int main(){
+    list<int>L{1,2,3};
+    cout<<"og List:"<<endl;
+    for(int i:L)
+    cout<<i<<endl;
+}    
