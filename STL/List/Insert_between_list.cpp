c9a417f477ac1a14fc092ba6e174c033301edba2
tusharmishra069 <mishratushar133@gmail.com>
2024-09-01 14:30:51 +0530

    insert in Between

---
+++ STL/List/Insert_between_list.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    list<int>l{1,2,3,4};
+    cout<<"insert in between the Iterator: ";
+    auto it = l.begin();
+    advance(it,2);//iterator standing in the pos 2
+    l.insert(it,33);
+    for(auto i:l){
+        cout<<i<<endl;
+    }
+}
