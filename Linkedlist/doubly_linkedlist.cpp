cf76030b51992683abba850d069f854b5d0460bf
tusharmishra069 <mishratushar133@gmail.com>
2024-09-11 23:30:57 +0530

    Doubly linked list

---
+++ Linkedlist/doubly_linkedlist.cpp
@@ -0,0 +1,78 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+        int data;
+        Node *next;
+        Node* prev;
+    Node()
+    {                                    
+        data=0;
+        next=NULL;
+        prev=NULL;
+    }
+    Node(int data){
+        this->data=data;
+        this->next=NULL;
+        this->prev=NULL;
+    }
+};
+    class linkedlist{
+        public:
+        Node *head;
+        linkedlist(){
+            head=NULL;
+        }
+        void insert(int data);
+        void insertatbeginning(int data);
+        void insertatposition(int data,int pos);
+        void display();
+    };
+    void linkedlist::insert(int data)
+    {
+       Node* newnode = new Node(data);
+        if(head==NULL)
+        {
+            head = newnode;
+            return;
+        }
+        Node* temp = head;
+        while(temp->next!=NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newnode;
+        newnode->prev=temp;
+    }
+     void linkedlist:: display(){
+        Node* temp = head;
+        if(head==NULL){
+            cout<<"list empty"<<endl;
+        }
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<" ";
+            temp = temp->next;
+        }
+    }
+    void linkedlist:: insertatbeginning(int data){
+        Node* newnode=new Node(data);
+        if(head==NULL)
+        {
+            head = newnode;
+            return;
+        } 
+        else{
+            newnode->next=head;
+            head->prev=newnode;
+            head=newnode;
+        }
+    }
+    int main(){
+        linkedlist obj;
+        obj.insert(100);
+        obj.insert(200);
+        obj.insert(300);
+        obj.insert(400);
+        obj.display();
+    }
