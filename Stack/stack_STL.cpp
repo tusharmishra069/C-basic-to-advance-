83c3b5749eca9af0d53e0e4321e329b976e39ba9
tusharmishra069 <mishratushar133@gmail.com>
2024-09-19 22:11:36 +0530

    upadate little

--- Stack/stack_STL.cpp
+++ Stack/stack_STL.cpp
@@ -10,8 +10,9 @@ int main(){
     s.push(10); //push is use to store the data 
     s.push(20);
     s.push(30);
-    cout << "Size of stack: " << s.size() << endl;
-    while(!s.empty()){
+    cout << "Size of stack: " << s.size() << endl;  //size  is use to find the size of stack
+
+    while(!s.empty()){ //empty is use to Print the top element and pop it from the stack
         cout<<s.top()<<" ";
         s.pop(); //pop remove the  data from the stack, and its remove from the top 
 
